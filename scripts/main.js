2daa95d482ee77ad0cc1c2284db848419e80ac7c
Alex Strugacz <alexstrugacz@icloud.com>
2022-08-09 03:33:22 -0500

    complete winner determination script

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,34 @@
-// insert javascript
+let userChoice = prompt("Please enter Rock, Paper, or Scissors!");
+const choiceArray = ["Rock", "Paper", "Scissors"];
+
+const computerChoosing = () => {
+  let randomChoice = Math.floor(Math.random() * choiceArray.length);
+  return choiceArray[randomChoice];
+};
+let computerChoice = computerChoosing();
+console.log(computerChoice);
+
+if (computerChoice === userChoice) {
+  console.log("It's a tie!");
+} else if (userChoice === "Rock") {
+  if (computerChoice === "Paper") {
+    console.log("The computer wins!");
+  }
+  if (computerChoice === "Scissors") {
+    console.log("The player wins!");
+  }
+} else if (userChoice === "Paper") {
+  if (computerChoice === "Rock") {
+    console.log("The computer wins!");
+  }
+  if (computerChoice === "Scissors") {
+    console.log("The computer wins!");
+  }
+} else if (userChoice === "Scissors") {
+  if (computerChoice === "Rock") {
+    console.log("The computer wins!");
+  }
+  if (computerChoice === "Paper") {
+    console.log("The player wins!");
+  }
+}
