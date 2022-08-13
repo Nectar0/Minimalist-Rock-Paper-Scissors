9e107197a39a2b57347c3e26d05e6cd5160c3c94
Alex Strugacz <alexstrugacz@icloud.com>
2022-08-12 21:53:51 -0500

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -9,32 +9,37 @@ const computerChoosing = () => {
   console.log(`The computer chooses ${choiceArray[randomChoice]}!`);
   return choiceArray[randomChoice];
 };
-
+//Script IF computer wins
 const computerWins = () => {
   console.log("The computer wins!");
   computerScore++;
 };
 
+//Script IF player wins
 const playerWins = () => {
   console.log("The player wins!");
   playerScore++;
 };
 
-// Recieve input from user
+// Receive input from user
 // Using randomized answer from computer
 // Determine winner and adjust score up, loop for a game of five
 const game = () => {
   for (let i = 0; i < 5; i++) {
+    //Prompt
     let userChoice = prompt("Please enter Rock, Paper, or Scissors!");
+    //Check user input valid answer
     if (
       userChoice === "Rock" ||
       userChoice === "Paper" ||
       userChoice === "Scissors"
     ) {
+      //Check for tie
       console.log(`The player chooses ${userChoice}!`);
       let computerChoice = computerChoosing();
       if (computerChoice === userChoice) {
         console.log("It's a tie!");
+        //Check all possibilities
       } else if (userChoice === "Rock") {
         if (computerChoice === "Paper") {
           computerWins();
@@ -60,6 +65,7 @@ const game = () => {
       console.log(playerScore, computerScore);
     } else console.log("Try something else!");
   }
+  //Determine winner
   if (playerScore > computerScore) {
     return "The player wins against the computer!";
   }
@@ -68,4 +74,5 @@ const game = () => {
   } else return "The game is a tie, try again!";
 };
 
+//Play game in console; temp;
 console.log(game());
